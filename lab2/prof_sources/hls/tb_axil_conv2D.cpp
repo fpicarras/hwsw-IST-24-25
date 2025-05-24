9f48a56827b798f31349796537ec337ccfd5e864
Joao-Pedro-Cabral <miranda.jp@usp.br>
2025-05-24 15:09:05 +0100

    Fixing conv2D and adding int tb files

--- lab2/prof_sources/hls/tb_axil_conv2D.cpp
+++ lab2/prof_sources/hls/tb_axil_conv2D.cpp
@@ -1,5 +1,6 @@
 
 #include "axil_conv2D.h"
+#include "tb_axil_conv2D_int.h"
 #include <stdint.h>
 
 static int8_t image_in[IMAGE_CHANNELS * IMAGE_HEIGHT * IMAGE_WIDTH];
@@ -14,66 +15,9 @@ static int8_t sw_image_out[CONV_OFM_NUMBER * CONV_OUTPUT_HEIGHT * CONV_OUTPUT_WI
  * @param matrix_in Input matrix
  * @param matrix_out Output matrix after convolution
  */
-void sw_convolution_2D(const int8_t *matrix_in, int8_t *matrix_out) {
-    for(int l = 0; l < CONV_OFM_NUMBER; l ++)
-        for (int i = 0; i < CONV_OUTPUT_HEIGHT; i++)
-            for (int j = 0; j < CONV_OUTPUT_WIDTH; j++) {
-                int accum = bias[l];                       /* initialize result with bias */
-
-                for (int k = 0; k < CONV_KERNEL_SIZE; k++)
-                    for (int x = 0; x < CONV_KERNEL_SIZE; x++) {
-                        /* Kernel index */
-                        int kernel_1d_idx = l*(CONV_KERNEL_SIZE*CONV_KERNEL_SIZE) + // kernel number
-                                k * CONV_KERNEL_SIZE +       /* kernel row */
-                                x;                      /* kernel column */
-
-                        /* Input matrix index */
-                        int input_1d_idx =
-                                (i + k) * IMAGE_WIDTH + /* input row */
-                                j + x;                  /* input column */
-
-                        accum += kernel[kernel_1d_idx] * matrix_in[input_1d_idx];
-                        accum += kernel[kernel_1d_idx] * matrix_in[input_1d_idx + IMAGE_HEIGHT * IMAGE_WIDTH];
-                        accum += kernel[kernel_1d_idx] * matrix_in[input_1d_idx + 2 * IMAGE_HEIGHT * IMAGE_WIDTH];
-                    }
-
-                /* Normalize result */
-                accum >>= 16;
-                if (accum > 127)
-                    accum = 127;
-                else if (accum < 0)
-                    accum = 0;
-
-                matrix_out[i * CONV_OUTPUT_WIDTH + j] = accum;
-            }
-}
 
 int main() {
-    printf("Input Image\n\r");
-    for(int k = 0; k < IMAGE_CHANNELS; k++) {
-        for (int i = 0; i < IMAGE_HEIGHT; i++) {
-            for (int j = 0; j < IMAGE_WIDTH; j++) {
-                image_in[k*IMAGE_HEIGHT*IMAGE_WIDTH + i * IMAGE_WIDTH + j] = (i + 1) * 30 + (j + 1)*3 + k;
-                printf("%4d ", image_in[k*IMAGE_HEIGHT*IMAGE_WIDTH + i * IMAGE_WIDTH + j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-    printf("Kernel\n\r");
-    for (int i = 0; i < CONV_OFM_NUMBER; i++) {
-        for (int j = 0; j < CONV_KERNEL_SIZE * CONV_KERNEL_SIZE; j++) {
-            kernel[i * CONV_KERNEL_SIZE * CONV_KERNEL_SIZE + j] = (1 << (14 + i)) + (1 << 13)*j;
-            printf("%6d ", kernel[i * CONV_KERNEL_SIZE * CONV_KERNEL_SIZE + j]);
-        }
-        printf("\n");
-    }
-    printf("Bias\n\r");
-    for (int i = 0; i < CONV_OFM_NUMBER; i++) {
-        bias[i] = (1 << 14);
-        printf("%6d ", bias[i]);
-    }
-    printf("\n");
+    init_inputs(image_in, kernel, bias);
 
     hls::stream<strmio_t> str_in;
     hls::stream<strmio_t> str_out;
@@ -107,36 +51,7 @@ int main() {
         hw_image_out[i + 3] = tmp_out.data(31, 24);
     }
 
-    sw_convolution_2D(image_in, sw_image_out);
+    sw_convolution_2D(image_in, kernel, bias, sw_image_out);
 
-    printf("Output Image SW\n\r");
-    for(int k = 0; k < CONV_OFM_NUMBER; k ++)
-        for (int i = 0; i < CONV_OUTPUT_HEIGHT; i++) {
-            for (int j = 0; j < CONV_OUTPUT_WIDTH; j++) {
-                printf("%4d ", sw_image_out[k * CONV_OUTPUT_HEIGHT * CONV_OUTPUT_WIDTH + i * CONV_OUTPUT_WIDTH + j]);
-            }
-            printf("\n\r");
-        }
-
-    printf("Output Image HW\n\r");
-    for(int k = 0; k < CONV_OFM_NUMBER; k ++)
-        for (int i = 0; i < CONV_OUTPUT_HEIGHT; i++) {
-            for (int j = 0; j < CONV_OUTPUT_WIDTH; j++) {
-                printf("%4d ", hw_image_out[k * CONV_OUTPUT_HEIGHT * CONV_OUTPUT_WIDTH + i * CONV_OUTPUT_WIDTH + j]);
-            }
-            printf("\n\r");
-        }
-
-    int err_cnt = 0;
-    for(int k = 0; k < CONV_OFM_NUMBER; k ++)
-        for (int i = 0; i < CONV_OUTPUT_HEIGHT; i++)
-            for (int j = 0; j < CONV_OUTPUT_WIDTH; j++) {
-                int ind = k * CONV_OUTPUT_HEIGHT * CONV_OUTPUT_WIDTH + i * CONV_OUTPUT_WIDTH + j;
-                if (hw_image_out[ind] != sw_image_out[ind]) {
-                    err_cnt++;
-                    printf("%d,%d: %d != %d\n\r", i, j, hw_image_out[ind], sw_image_out[ind]);
-                }
-            }
-
-    return err_cnt;
+    return check_output(sw_image_out, hw_image_out);
 }
---
+++ lab2/prof_sources/hls/tb_axil_conv2D_int.h
@@ -0,0 +1,13 @@
+
+#ifndef __TB_AXIL_CONV3D_H__
+#define __TB_AXIL_CONV3D_H__
+
+#include <stdint.h>
+
+void init_inputs(int8_t *image_in, int16_t * kernel, int16_t * bias);
+
+void sw_convolution_2D(const int8_t *matrix_in, const int16_t * kernel, const int16_t * bias, int8_t *matrix_out);
+
+int check_output(const int8_t *sw_matrix_out, int8_t *hw_matrix_out);
+
+#endif // __TB_AXIL_CONV3D_H__
